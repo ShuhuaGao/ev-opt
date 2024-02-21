8f6ed88fedf2a63700d845635b9f5df4fe573639
Gao Shuhua <shuhua_gao@u.nus.edu>
2024-02-21 14:01:15 +0800

    couenne worked

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # ev-opt
 Optimization for EV charging.
 
-Change into the root directory and run "nlp.jl".
+## How to use
+1. Set the [`JULIA_NUM_THREADS`](https://docs.julialang.org/en/v1/manual/environment-variables/#JULIA_NUM_THREADS) environment variable on your system to a proper number (usually the number of cores). 
+   Note that by default its value is 1, that is, only one core is used during Julia execution even if you do parallel computing.
+2. Start Julia REPL, and set up the environment for ev-opt in Julia REPL. Recall the usage of `cd()`, `]`, `activate .`  and `update`
+3. Edit the "nlp.jl" file to change the inputs to the `main` call.
+4. Edit the Couenne option file "couenne.opt"
+5. In Julia REPL, run `include("nlp.jl")`.
+6. Once finished, the result of *input.npz* data is stored in *input-res.npz*.
--- nlp.jl
+++ nlp.jl
@@ -1,5 +1,7 @@
-using JuMP, SCIP, Ipopt, EAGO
-using Alpine, HiGHS
+using JuMP, Ipopt
+using AmplNLWriter, Couenne_jll
+using Dates
+
 
 include("EV.jl")
 
@@ -14,7 +16,7 @@ function build_model(EVs::EVData, ρ::AbstractVector{Float64}; Δt::Float64, β:
     ta_min = minimum(EVs.ta)
     td_max = maximum(EVs.td)
     T = td_max - ta_min     # because td is excluded 
-    @show T length(ρ)
+    # @show T length(ρ)
     @assert length(ρ) == T
     @assert 0 <= β <= 1
     @assert Plim > 0
@@ -25,6 +27,7 @@ function build_model(EVs::EVData, ρ::AbstractVector{Float64}; Δt::Float64, β:
     K = length(EVs.SoCmin)    # K vehicles
 
     model = JuMP.Model()
+
     @variable(model, 0 <= a[1:K, 1:T] <= 1.0)
     @expression(model, P, a .* EVs.Pmax)        # K×T
     # power before ta or after and on td must be enforced to be 0
@@ -57,22 +60,17 @@ function build_model(EVs::EVData, ρ::AbstractVector{Float64}; Δt::Float64, β:
         ts = EVs.ta[k]:EVs.td[k]-1  # valid time range for EV k 
         g[k, ts] .= EVs.C[k] .* (EVs.SoCd[k] .- SoC[k, ts]) ./ (EVs.Pmax[k] .* Δt .* EVs.e[k])  ./ (EVs.td[k] .- ts)
     end
-    @expression(model, G, g ./ (sum(g; dims=1) .+ 1e-8))  # K × T, 1e-8 avoids zero division 
+    @expression(model, G, g ./ (sum(g; dims=1) .+ 1e-6))  # K × T, 1e-8 avoids zero division 
     @expression(model, f_obj, abs.(P .- sum(P; dims=1) .* G))  # K × T
 
     # total objective: weighted sum 
     @objective(model, Min, sum((1 - β) .* c_obj .+ β .* f_obj))
-
-    # uncomment the following lines to move the nonlinear `f_obj` into constraints
-    # @variable(model, f_obj_aux)
-    # @constraint(model, sum(f_obj) <= f_obj_aux)
-    # @objective(model, Min, (1 - β) * sum(c_obj) + β * f_obj_aux)
     
     return model
 end
 
 
-function solve!(model::JuMP.Model, optimizer::Symbol=:SCIP)
+function solve!(model::JuMP.Model; optimizer::Symbol=:Couenne)
     if optimizer == :SCIP
         set_optimizer(model, SCIP.Optimizer)
     elseif optimizer == :Alpine
@@ -83,26 +81,54 @@ function solve!(model::JuMP.Model, optimizer::Symbol=:SCIP)
             "nlp_solver" => ipopt,
             "mip_solver" => highs,
         ))
+    elseif optimizer == :Ipopt
+        set_optimizer(model, Ipopt.Optimizer)
+        set_attribute(model, "tol", 1e-6)
+        set_attribute(model, "acceptable_tol", 1e-4)
+    elseif optimizer == :Couenne
+        set_optimizer(model, () -> AmplNLWriter.Optimizer(Couenne_jll.amplexe))
     else
         error("Unsupported optimizer")
     end
+
     optimize!(model)
 
-    if termination_status(model) == JuMP.OPTIMAL
-        return model
-    else
-        error("Optimization failed: $(termination_status(model))")
+    @show termination_status(model)
+    if termination_status(model) ∈ [JuMP.INFEASIBLE, JuMP.INFEASIBLE_OR_UNBOUNDED]
+        @error "Infeasible"
     end
-    
+    @show objective_value(model)
+
+    return model
 end
 
 
-function main()
-    EVs = EVData("./data/EVDATA.npz")
-    cfg = npzread("./data/EVDATA.npz")
+function run(npz_file::String)
+    EVs = EVData(npz_file)
+    cfg = npzread(npz_file)
     model = build_model(EVs, cfg["rho"]; Δt=cfg["delta_t"], β=cfg["b"], Plim=cfg["Plim"])
-    solve!(model, :SCIP)
+    try
+        solve!(model)
+        # store results
+        keys = ["a", "P", "g", "G", "SoC", "c_obj", "f_obj"]
+        res = Dict{String, Union{Matrix{Float64}, Float64}}(k=>value.(model[Symbol(k)]) for k in keys)
+        res["obj_value"] = objective_value(model)
+        target_npz_file = npz_file[1:end-4] * "-res.npz"
+        npzwrite(target_npz_file, res)
+        println("Optimization towards $npz_file finished!")
+    catch e
+        println("Unable to optimize $npz_file")
+    end
 end
 
 
-main()
+function main(npz_files::AbstractVector{String})
+    @show Threads.nthreads()        # number of threads available
+    Threads.@threads for npz_file in npz_files
+        run(npz_file)
+    end
+end
+
+
+# process a list of input data in parallel
+main(["./data/EVDATA1.npz", "./data/EVDATA2.npz", "./data/EVDATA3.npz"])
